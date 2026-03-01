8aec5b02c9d4bbbc2c7bd9a510bea3cd8baa7fee
Sivanijaya <166210126+Sivanijaya@users.noreply.github.com>
2026-03-01 20:35:37 +0530

    Add files via upload

---
+++ PythonProject/Band Name Generator(1).py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("what city you grew up?")
+pet = input("what is your pet name?")
+print("Your band name could be:" + city +" " + pet)
---
+++ PythonProject/hello.py
@@ -0,0 +1,20 @@
+# print("hello world")
+
+  ## print("hello " + input("what is your name?") +"!")
+
+  ## name = input("what is your name")
+  ## print(name)
+
+  ## print(len(input("what is your name")))
+
+ ## print("hello"[4])## it shows that inside the  square  brackets index position of the character that we want
+## street_name = "Abbey Road"
+## print(street_name[4] + street_name[7])
+
+
+
+
+
+
+
+
